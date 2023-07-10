c6281968a31f2dfd94471ea69fab4c1483b457d5
Katerina <emrodionova@bk.ru>
2023-07-10 20:22:09 +0300

    added abstract methods to task

---
+++ notes/main.py
@@ -0,0 +1,17 @@
+"""
+Задание 1. Приложение заметки (Python)
+Необходимо написать проект, содержащий функционал работы с заметками.
+Программа должна уметь создавать заметку, сохранять её, читать список заметок,
+редактировать заметку, удалять заметку.
+"""
+
+#from controller.Controller import Controller
+
+def main():
+    print('this is my project')
+    #controller = Controller()
+    #controller.start()
+
+
+if __name__ == '__main__':
+    main()
